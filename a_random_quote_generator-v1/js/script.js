f8b76c67e891d0c7d3e48b98f61e2b50b3ba58c4
karla.ortiz <karla.ortiz.martinez47@gmail.com>
2023-09-10 23:27:13 -0700

    created getRandomQuote function

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -10,19 +10,51 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
-
+const quotes = [
+  {
+    quote: "Optimism is the faith that leads to achievement. Nothing can be done without hope and confidence.",
+    source: 'Helen Keller',
+    year: 1903
+  },
+  {
+    quote:"Our lives aren't just measured in years, they're measured in the lives of the people we touch around us." ,
+    source: 'Peeta Mellark',
+    citation:'The Hunger Games: Catching Fire',
+    year: 2013
+  },
+  {
+    quote: "Life is 10% what happens to you and 90% how you react to it.",
+    source: "Charles R. Swindoll"
+  },
+  {
+    quote: "Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.",
+    source: "Thomas A. Edison",
+    year: 1879
+  },
+  {
+    quote: "If you can't change reality, change your perception of it.",
+    source: "Audre Lorde",
+    citation: "Zami: A New Spelling of My Name",
+    year: 1982
+  }
+];
 
 /***
  * `getRandomQuote` function
 ***/
-
+function getRandomQuote(){
+  const randomNum = Math.floor(Math.random()*quotes.length)+1;
+  const randomQuote = quotes[randomNum];
+  return randomQuote;
+}
 
 
 /***
  * `printQuote` function
 ***/
-
+function printQuote(){
+  const randomQuote = getRandomQuote();
+}
 
 
 /***
